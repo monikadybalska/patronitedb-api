89be593895d96fb36b060ceccf8e34ca57080d79
Monika <monika.dybalska@hotmail.com>
2024-07-16 21:04:50 +0200

    add data writes

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,12 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { parseNumber } from './lib/utils';
 
-describe('AppController', () => {
-  let appController: AppController;
-
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
-
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+describe('Number parsing', () => {
+  it('should parse correctly', () => {
+    expect(parseNumber('7112')).toBe(7112);
+    expect(parseNumber('112430 zł')).toBe(112430);
+    expect(parseNumber('4.23 mln zł')).toBe(4230000);
+    expect(parseNumber('40 mln zł')).toBe(40000000);
+    expect(parseNumber('197 tys. zł')).toBe(197000);
+    expect(parseNumber('')).toBe(-1);
   });
 });
